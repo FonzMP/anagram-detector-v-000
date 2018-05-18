1459ab9bba4497e1ca55efc6cbf4ea6a7a2a72f6
Alex Wilson <fonzmp@gmail.com>
2018-05-18 07:16:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
     holder = []
     list.each do |check|
       binding.pry
-      if @word == check || @word == check.reverse!
+      if @word == check || check.reverse! == word
         holder << check
       else
         holder = []
